78ea7debf36cf40b3ea88a4ca2347d6d8ea4801f
Fatma <Fatma@DESKTOP-Q3FGQH0>
2021-03-24 15:46:58 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Collections.Generic.Dictionary<int, string> MyDictionary = new System.Collections.Generic.Dictionary<int, string>();
+            MyDictionary.Add(01, "C#");
+            MyDictionary.Add(02, "Java");
+            MyDictionary.Add(03, "Python");
+            MyDictionary.Add(04, "Angular");
+            Console.WriteLine(MyDictionary.Count);
+        }
+    }
+}
